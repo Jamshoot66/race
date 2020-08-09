13c53320ba046739172e682b13b5d179ce79cffb
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-09 21:53:18 +0400

    add style loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -29,6 +29,14 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
 };
