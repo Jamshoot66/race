c7c195ba4f2d527d7074f345a964ba7b6b5e9e43
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-12 20:43:40 +0400

    add keyboard events

--- src/index.js
+++ src/index.js
@@ -5,13 +5,27 @@ window.addEventListener('load', () => {
   const core = new Core({
     containerId: 'screen-render-container',
   });
-  
+
   document.getElementById('btn-left').addEventListener('click', () => {
-    core.processPlayerControl('moveLeft')
-  })
-  
+    core.processPlayerControl('moveLeft');
+  });
+
   document.getElementById('btn-right').addEventListener('click', () => {
-    core.processPlayerControl('moveRight')
-  })
-  
+    core.processPlayerControl('moveRight');
+  });
+
+  window.addEventListener('keydown', (e) => {
+    switch (e.code) {
+      case 'ArrowLeft': {
+        core.processPlayerControl('moveLeft');
+        break;
+      }
+      case 'ArrowRight': {
+        core.processPlayerControl('moveRight');
+        break;
+      }
+      default:
+        return;
+    }
+  });
 });
