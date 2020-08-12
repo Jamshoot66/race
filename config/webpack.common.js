4b6efb8c22d79674f0a6bbedf19cee0c28cf26e8
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-12 22:12:19 +0400

    add utils alias

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -23,6 +23,7 @@ module.exports = {
     alias: {
       objects: path.resolve(__dirname, '../src/objects/'),
       config: path.resolve(__dirname, '../src/config/'),
+      utils: path.resolve(__dirname, '../src/utils/'),
       src: path.resolve(__dirname, '../src/'),
     },
     extensions: ['.js'],
