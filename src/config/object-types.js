99fa96ee0dc9cb1c63c243c5afbef44662819a6d
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-13 16:55:48 +0400

    add enemy car creator

--- src/config/object-types.js
+++ src/config/object-types.js
@@ -1,3 +1,10 @@
 export const TYPE_PLAYER = 'TYPE_PLAYER';
 export const TYPE_ENEMY_CAR = 'TYPE_ENEMY_CAR';
 export const DEFAULT_PLAYER_COLOR = 0xF62C1B;
+
+export const CAR_COLORS = [
+  0x2F2E38,
+  0x708323,
+  0x5A7D83,
+  0x6482F0
+]
--- src/objects/Car2D/Car2D.factory.js
+++ src/objects/Car2D/Car2D.factory.js
@@ -1,6 +1,7 @@
 import Car2D from './Car2D.js';
 import * as constants from 'config/constants';
 import * as types from 'config/object-types';
+import { getRandomItem } from 'utils/math';
 
 export const createCar = ({
   type,
@@ -17,4 +18,11 @@ export const createPlayerCar = () =>
     type: types.TYPE_PLAYER,
     color: types.DEFAULT_PLAYER_COLOR,
     position: { x: 0.0, y: -8.5 * constants.BLOCK_SIZE, z: 0.0 },
+  });
+
+export const createEnemyCar = () =>
+  createCar({
+    type: types.TYPE_ENEMY_CAR,
+    color: getRandomItem(types.CAR_COLORS),
+    position: { x: 0.0, y: constants.TOP_POSITION, z: 0.0 },
   });
