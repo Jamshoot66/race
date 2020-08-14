a0f3db62efa860cadf6aac3d0e5bf2ec22fb0816
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-14 08:11:36 +0400

    fix production build static files

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -1,6 +1,19 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    publicPath: '/',
+  },
+  
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/sound', to: 'sound' },
+        { from: 'public/textures', to: 'textures' },
+      ],
+    }),
+  ],
 });
